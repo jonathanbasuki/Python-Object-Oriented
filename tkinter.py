1de25b7a9f58a6a9ebd284e29b279c103a5e6c1a
jonathanbasuki <56567659+jonathanbasuki@users.noreply.github.com>
2020-08-22 12:49:17 +0700

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -5,12 +5,12 @@ main_window = tkinter.Tk()
 label1 = tkinter.Label(main_window, text = "Name")
 label2 = tkinter.Label(main_window, text = "Email")
 
-tombol1 = tkinter.Button(main_window, text = "Submit")
-tombol2 = tkinter.Button(main_window, text = "Cancel")
+button1 = tkinter.Button(main_window, text = "Submit")
+button2 = tkinter.Button(main_window, text = "Cancel")
 
 label1.pack()
 label2.pack()
-tombol1.pack()
-tombol2.pack()
+button1.pack()
+button2.pack()
 
 main_window.mainloop()
